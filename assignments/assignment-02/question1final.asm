ae3a9a5edb7bb7c13a2c660514700a1f8956e60a
halcyoona <halcyoona@gmail.com>
2018-10-14 15:47:55 +0500

    good

---
+++ assignments/assignment-02/question1final.asm
@@ -0,0 +1,127 @@
+include irvine32.inc
+
+
+;print values in inverted form
+
+.data
+	var1 DT 0AABBBBCCCCDDDDEEFFFFh
+	var2 DT	0AABBBBCCCCDDDDEEFFFFh
+	check	dword 0
+	sum  word 0, 0, 0, 0, 0, 0 
+	;final DT 0
+	;multiply byte "a",0
+.code
+
+	
+	Main proc
+	
+	;First FFFF + FFFF
+	movzx eax, word PTR var1
+	mov check, eax
+	movzx eax, word PTR var2
+	add check, eax
+
+	xor eax, eax
+	mov ax, word PTR check
+	mov sum+10, ax
+	;call writehex
+	
+	
+	
+	;second DDEE + DDEE
+	xor eax, eax
+	xor ebx, ebx
+	movzx ebx, word PTR check+2
+	movzx eax, word PTR var1+2
+	mov check, eax
+	movzx eax, word PTR var2+2
+	add check, eax
+	add check, ebx
+	mov eax, check
+	;call writehex
+	
+	xor eax, eax
+	mov ax, word PTR check
+	mov sum+8, ax
+	;call writehex
+	
+	
+	
+	
+	;CCDD + CCDD
+	xor eax, eax
+	xor ebx, ebx
+	movzx ebx, word PTR check+2
+	movzx eax, word PTR var1+4
+	mov check, eax
+	movzx eax, word PTR var2+4
+	add check, eax
+	add check, ebx
+	mov eax, check
+	;call writehex
+		
+	xor eax, eax
+	mov ax, word PTR check
+	mov sum+6, ax
+	;call writehex
+	
+	
+	;BBCC + BBCC
+	xor eax, eax
+	xor ebx, ebx
+	movzx ebx, word PTR check+2
+	movzx eax, word PTR var1+6
+	mov check, eax
+	movzx eax, word PTR var2+6
+	add check, eax
+	add check, ebx
+	mov eax, check
+	;call writehex
+			
+	xor eax, eax
+	mov ax, word PTR check
+	mov sum+4, ax
+	;call writehex
+	
+	
+	
+	
+	;AABB + AABB
+	xor eax, eax
+	xor ebx, ebx
+	movzx ebx, word PTR check+2
+	movzx eax, word PTR var1+8
+	mov check, eax
+	movzx eax, word PTR var2+8
+	add check, eax
+	add check, ebx
+	mov eax, check
+	;call writehex
+			
+	xor eax, eax
+	mov ax, word PTR check
+	mov sum+2, ax
+	;call writehex
+
+
+	;last 1
+	mov ax, word PTR check+2
+	mov sum+0, ax
+	;call writehex
+	
+	
+	xor eax, eax
+	mov eax, dword PTR sum+0
+	call writehex
+	mov eax, dword PTR sum+4
+	call writehex
+	mov eax, dword PTR sum+8
+	call writehex
+	call crlf
+
+	
+
+	
+	exit
+	Main endp
+	End Main
